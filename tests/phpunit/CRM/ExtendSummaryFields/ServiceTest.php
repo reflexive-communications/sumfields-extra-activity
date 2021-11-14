d40fa299e7537c053bdbb8c2eba252903dd25d8b
akosgarai <szlem01@gmail.com>
2021-11-14 14:57:26 +0100

    Implement service tests

--- tests/phpunit/CRM/ExtendSummaryFields/ServiceTest.php
+++ tests/phpunit/CRM/ExtendSummaryFields/ServiceTest.php
@@ -6,16 +6,7 @@ use Civi\Test\HookInterface;
 use Civi\Test\TransactionalInterface;
 
 /**
- * FIXME - Add test description.
- *
- * Tips:
- *  - With HookInterface, you may implement CiviCRM hooks directly in the test class.
- *    Simply create corresponding functions (e.g. "hook_civicrm_post(...)" or similar).
- *  - With TransactionalInterface, any data changes made by setUp() or test****() functions will
- *    rollback automatically -- as long as you don't manipulate schema or truncate tables.
- *    If this test needs to manipulate schema or truncate tables, then either:
- *       a. Do all that using setupHeadless() and Civi\Test.
- *       b. Disable TransactionalInterface, and handle all setup/teardown yourself.
+ * Testcases for Service class.
  *
  * @group headless
  */
@@ -23,37 +14,107 @@ class CRM_ExtendSummaryFields_ServiceTest extends \PHPUnit\Framework\TestCase im
 {
     public function setUpHeadless()
     {
-        // Civi\Test has many helpers, like install(), uninstall(), sql(), and sqlFile().
-        // See: https://docs.civicrm.org/dev/en/latest/testing/phpunit/#civitest
         return \Civi\Test::headless()
-      ->installMe(__DIR__)
-      ->apply();
+            ->install('net.ourpowerbase.sumfields')
+            ->installMe(__DIR__)
+            ->apply();
     }
 
-    public function setUp()
+    /**
+     * Apply a forced rebuild of DB, thus
+     * create a clean DB before running tests
+     *
+     * @throws \CRM_Extension_Exception_ParseException
+     */
+    public static function setUpBeforeClass(): void
+    {
+        // Resets DB and install depended extension
+        \Civi\Test::headless()
+            ->install('net.ourpowerbase.sumfields')
+            ->installMe(__DIR__)
+            ->apply(true);
+    }
+
+    /**
+     * Create a clean DB after running tests
+     *
+     * @throws CRM_Extension_Exception_ParseException
+     */
+    public static function tearDownAfterClass(): void
+    {
+        \Civi\Test::headless()
+            ->uninstallMe(__DIR__)
+            ->uninstall('net.ourpowerbase.sumfields')
+            ->apply(true);
+    }
+
+    public function setUp(): void
     {
         parent::setUp();
     }
 
-    public function tearDown()
+    public function tearDown(): void
     {
         parent::tearDown();
     }
 
-    /**
-     * Example: Test that a version is returned.
+    /*
+     * It tests the sumfieldsDefinition function.
      */
-    public function testWellFormedVersion()
+    public function testSumfieldsDefinition()
     {
-        $this->assertNotEmpty(E::SHORT_NAME);
-        $this->assertRegExp('/^([0-9\.]|alpha|beta)*$/', \CRM_Utils_System::version());
+        $definitions = [];
+        self::assertEmpty(CRM_ExtendSummaryFields_Service::sumfieldsDefinition($definitions));
+        // the definition list has to be extended.
+        self::assertTrue(array_key_exists('fields', $definitions));
+        self::assertTrue(array_key_exists('optgroups', $definitions));
+        self::assertCount(6, $definitions['fields']);
+        self::assertCount(1, $definitions['optgroups']);
+        self::assertTrue(array_key_exists('extend_summary_fields', $definitions['optgroups']));
     }
-
-    /**
-     * Example: Test that we're using a fake CMS.
+    /*
+     * It tests the buildForm function.
      */
-    public function testWellFormedUF()
+    public function testBuildFormDoesNothingWhenTheFormIsIrrelevant()
     {
-        $this->assertEquals('UnitTests', CIVICRM_UF);
+        self::assertEmpty(CRM_ExtendSummaryFields_Service::buildForm('irrelevant-form-name', new CRM_Sumfields_Form_SumFields()));
+    }
+    public function testBuildForm()
+    {
+        $form = new CRM_Sumfields_Form_SumFields();
+        $form->setVar('fieldsets', []);
+        self::assertEmpty(CRM_ExtendSummaryFields_Service::buildForm(CRM_Sumfields_Form_SumFields::class, $form));
+        self::assertEmpty(CRM_ExtendSummaryFields_Service::buildForm('Not_Relevant_Class_Name', $form));
+    }
+    /**
+     * Test the postProcess function.
+     */
+    public function testPostProcessDoesNothingWhenTheFormIsIrrelevant()
+    {
+        self::assertEmpty(CRM_ExtendSummaryFields_Service::postProcess('irrelevant-form-name', new CRM_Sumfields_Form_SumFields()));
+    }
+    public function testPostProcessNotOnSubmit()
+    {
+        $form = new CRM_Sumfields_Form_SumFields();
+        $form->setVar('when_to_apply_change', 'later');
+        $form->setVar('extend_summary_fields_activity_type_ids', []);
+        $form->setVar('extend_summary_fields_activity_status_ids', []);
+        $form->setVar('extend_summary_fields_record_type_id', []);
+        self::assertEmpty(CRM_ExtendSummaryFields_Service::postProcess(CRM_Sumfields_Form_SumFields::class, $form));
+    }
+    public function testPostProcessOnSubmit()
+    {
+        $form = new CRM_Sumfields_Form_SumFields();
+        $expectedActivityTypeIds = array_keys(CRM_Activity_BAO_Activity::buildOptions('activity_type_id', 'get'));
+        $expectedActivityStatusIds = array_keys(CRM_Activity_BAO_Activity::buildOptions('activity_status_id', 'get'));
+        $expectedContactRecortId = [array_keys(CRM_Activity_BAO_ActivityContact::buildOptions('record_type_id', 'get'))[0]];
+        $form->setVar('when_to_apply_change', 'on_submit');
+        $form->setVar('extend_summary_fields_activity_type_ids', $expectedActivityTypeIds);
+        $form->setVar('extend_summary_fields_activity_status_ids', $expectedActivityStatusIds);
+        $form->setVar('extend_summary_fields_record_type_id', $expectedContactRecortId);
+        self::assertEmpty(CRM_ExtendSummaryFields_Service::postProcess(CRM_Sumfields_Form_SumFields::class, $form));
+        self::assertSame($expectedActivityTypeIds, sumfields_get_setting('extend_summary_fields_activity_type_ids'));
+        self::assertSame($expectedActivityStatusIds, sumfields_get_setting('extend_summary_fields_activity_status_ids'));
+        self::assertSame($expectedContactRecortId, sumfields_get_setting('extend_summary_fields_record_type_id'));
     }
 }
