6d53bf4b6b0d17bc3d402696d7318d8caef47aa0
akosgarai <szlem01@gmail.com>
2021-11-14 15:27:05 +0100

    Extend test from base class

--- tests/phpunit/CRM/ExtendSummaryFields/ServiceTest.php
+++ tests/phpunit/CRM/ExtendSummaryFields/ServiceTest.php
@@ -1,66 +1,14 @@
 <?php
 
 use CRM_ExtendSummaryFields_ExtensionUtil as E;
-use Civi\Test\HeadlessInterface;
-use Civi\Test\HookInterface;
-use Civi\Test\TransactionalInterface;
 
 /**
  * Testcases for Service class.
  *
  * @group headless
  */
-class CRM_ExtendSummaryFields_ServiceTest extends \PHPUnit\Framework\TestCase implements HeadlessInterface, HookInterface, TransactionalInterface
+class CRM_ExtendSummaryFields_ServiceTest extends CRM_ExtendSummaryFields_HeadlessBase
 {
-    public function setUpHeadless()
-    {
-        return \Civi\Test::headless()
-            ->install('net.ourpowerbase.sumfields')
-            ->install('rc-base')
-            ->installMe(__DIR__)
-            ->apply();
-    }
-
-    /**
-     * Apply a forced rebuild of DB, thus
-     * create a clean DB before running tests
-     *
-     * @throws \CRM_Extension_Exception_ParseException
-     */
-    public static function setUpBeforeClass(): void
-    {
-        // Resets DB and install depended extension
-        \Civi\Test::headless()
-            ->install('net.ourpowerbase.sumfields')
-            ->install('rc-base')
-            ->installMe(__DIR__)
-            ->apply(true);
-    }
-
-    /**
-     * Create a clean DB after running tests
-     *
-     * @throws CRM_Extension_Exception_ParseException
-     */
-    public static function tearDownAfterClass(): void
-    {
-        \Civi\Test::headless()
-            ->uninstallMe(__DIR__)
-            ->uninstall('rc-base')
-            ->uninstall('net.ourpowerbase.sumfields')
-            ->apply(true);
-    }
-
-    public function setUp(): void
-    {
-        parent::setUp();
-    }
-
-    public function tearDown(): void
-    {
-        parent::tearDown();
-    }
-
     /*
      * It tests the sumfieldsDefinition function.
      */
