3e34d22eb997939d0dd4e3eff7b3daa5858e0b67
Sandor Semsey <sandor@es-progress.hu>
2022-03-19 18:25:26 +0100

    class: service

--- CRM/SumfieldsAddonActivity/Service.php
+++ CRM/SumfieldsAddonActivity/Service.php
@@ -1,11 +1,12 @@
 <?php
 
-use CRM_ActivitySumfields_ExtensionUtil as E;
+use CRM_SumfieldsAddonActivity_ExtensionUtil as E;
 
-class CRM_ActivitySumfields_Service
+class CRM_SumfieldsAddonActivity_Service
 {
-    const MULTIPLE_OPTIONS = ['multiple' => true, 'class' => 'crm-select2 huge'];
-    const SINGLE_OPTIONS = ['multiple' => false, 'class' => 'crm-select2 huge'];
+    public const MULTIPLE_OPTIONS = ['multiple' => true, 'class' => 'crm-select2 huge'];
+    public const SINGLE_OPTIONS = ['multiple' => false, 'class' => 'crm-select2 huge'];
+
     /**
      * This function extends the sumfield definition list with
      * our ones.
@@ -15,12 +16,13 @@ class CRM_ActivitySumfields_Service
         self::sumfieldsDefinitionNumberOfActivities($custom);
         self::sumfieldsDefinitionLatestActivity($custom);
     }
+
     public static function buildForm($formName, &$form)
     {
         if ($formName !== 'CRM_Sumfields_Form_SumFields') {
             return;
         }
-        $settings = new CRM_ActivitySumfields_Config(E::LONG_NAME);
+        $settings = new CRM_SumfieldsAddonActivity_Config(E::LONG_NAME);
         $labels = [
             'activity-type' => E::ts('Activity Types'),
             'activity-type-desc' => E::ts('Activity types to be used when calculating activity summary fields.'),
@@ -66,6 +68,7 @@ class CRM_ActivitySumfields_Service
 
         $form->assign('fieldsets', $fieldsets);
     }
+
     /**
      * This function handles the save process of the activity config.
      *
@@ -76,14 +79,19 @@ class CRM_ActivitySumfields_Service
     {
         if ($formName == 'CRM_Sumfields_Form_SumFields') {
             // Save option fields as submitted.
-            $settings = new CRM_ActivitySumfields_Config(E::LONG_NAME);
-            $settings->updateSetting('activity_sumfields_activity_type_ids', CRM_Utils_Array::value('activity_sumfields_activity_type_ids', $form->_submitValues));
-            $settings->updateSetting('activity_sumfields_date_activity_type_ids', CRM_Utils_Array::value('activity_sumfields_date_activity_type_ids', $form->_submitValues));
-            $settings->updateSetting('activity_sumfields_activity_status_ids', CRM_Utils_Array::value('activity_sumfields_activity_status_ids', $form->_submitValues));
-            $settings->updateSetting('activity_sumfields_date_activity_status_ids', CRM_Utils_Array::value('activity_sumfields_date_activity_status_ids', $form->_submitValues));
+            $settings = new CRM_SumfieldsAddonActivity_Config(E::LONG_NAME);
+            $settings->updateSetting('activity_sumfields_activity_type_ids',
+                CRM_Utils_Array::value('activity_sumfields_activity_type_ids', $form->_submitValues));
+            $settings->updateSetting('activity_sumfields_date_activity_type_ids',
+                CRM_Utils_Array::value('activity_sumfields_date_activity_type_ids', $form->_submitValues));
+            $settings->updateSetting('activity_sumfields_activity_status_ids',
+                CRM_Utils_Array::value('activity_sumfields_activity_status_ids', $form->_submitValues));
+            $settings->updateSetting('activity_sumfields_date_activity_status_ids',
+                CRM_Utils_Array::value('activity_sumfields_date_activity_status_ids', $form->_submitValues));
             // As the record type id is a single select, the string value is returned not an array.
             $settings->updateSetting('activity_sumfields_record_type_id', [CRM_Utils_Array::value('activity_sumfields_record_type_id', $form->_submitValues)]);
-            $settings->updateSetting('activity_sumfields_date_record_type_id', [CRM_Utils_Array::value('activity_sumfields_date_record_type_id', $form->_submitValues)]);
+            $settings->updateSetting('activity_sumfields_date_record_type_id',
+                [CRM_Utils_Array::value('activity_sumfields_date_record_type_id', $form->_submitValues)]);
 
             if ($form->_submitValues['when_to_apply_change'] == 'on_submit') {
                 $returnValues = [];
@@ -99,7 +107,7 @@ class CRM_ActivitySumfields_Service
      */
     private static function rewriteSql($sql): string
     {
-        $settings = new CRM_ActivitySumfields_Config(E::LONG_NAME);
+        $settings = new CRM_SumfieldsAddonActivity_Config(E::LONG_NAME);
         // Note: most of these token replacements fill in a sql IN statement,
         // e.g. field_name IN (%token). That means if the token is empty, we
         // get a SQL error. So... for each of these, if the token is empty,
