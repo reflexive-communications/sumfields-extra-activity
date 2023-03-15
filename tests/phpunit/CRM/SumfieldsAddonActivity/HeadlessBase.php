18534fca1cc7a6a6a84f813179bc92ffb1a7716f
Sandor Semsey <sandor@es-progress.hu>
2023-03-15 21:09:51 +0100

    remove tearDown methods

--- tests/phpunit/CRM/SumfieldsAddonActivity/HeadlessBase.php
+++ tests/phpunit/CRM/SumfieldsAddonActivity/HeadlessBase.php
@@ -35,28 +35,4 @@ class CRM_SumfieldsAddonActivity_HeadlessBase extends \PHPUnit\Framework\TestCas
             ->install('net.ourpowerbase.sumfields')
             ->apply(true);
     }
-
-    /**
-     * Create a clean DB after running tests
-     *
-     * @throws CRM_Extension_Exception_ParseException
-     */
-    public static function tearDownAfterClass(): void
-    {
-        \Civi\Test::headless()
-            ->uninstallMe(__DIR__)
-            ->uninstall('rc-base')
-            ->uninstall('net.ourpowerbase.sumfields')
-            ->apply(true);
-    }
-
-    public function setUp(): void
-    {
-        parent::setUp();
-    }
-
-    public function tearDown(): void
-    {
-        parent::tearDown();
-    }
 }
